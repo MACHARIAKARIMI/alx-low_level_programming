f249648de812b7e3a5a4bd6b8b74a98377f256eb
MACHARIAKARIMI <macharia93karimi@gmail.com>
2023-09-08 10:32:26 -0700

    Add C program to determine if a number is positive, negative, or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("The number %d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("The number %d is negative\n", n);
+	}
+	else
+	{
+	printf(printf("The number %d is zero\n", n);
+	}
+	return (0);
+}
